47126fc8b61eaa36738c57cb457c2660981ae7b8
danisnowman <danisnowman@gmail.com>
2018-07-28 09:40:50 -0600

    Finally working
    Removed askChar() and did what it was supposed to do manually in main.c and got all of the current switch options working

--- Warmup 3/main.c
+++ Warmup 3/main.c
@@ -10,37 +10,47 @@
 #include "commonFunctions.h"
 #include "structs.h"
 
-int counter = 0;				// counter is used to loop through all of the students and maxStudents defines the total amount of students that the admin will be able to enter.
-bool done;
-char studentName[maxLength];
+int counter = 0, id;	// counter is used to loop through all of the students and maxStudents defines the total amount of students that the admin will be able to enter.
+bool done;				// used for finishing program
+bool noMatch = true;			// if no match is found on the 3rd option of the switch
+char studentName[maxLength];	// Array of students, used to have multiple students
 int main(int argc, const char * argv[]) {
 	int maxStudents = askInt("Hello! \nPlease enter how many students you wish to enter \n\nStudents: ", 256);
 	struct saveStudent student[maxStudents];	// new array of the object type saveStudent. This is each student's personal info.
-	while(!done){
-	int switchInput = askInt("Please choose one of the following: \n\n1. Enter student's info. \n2. Print all the info saved. \n3. Search student by ID. \n4. Exit.\n\n Option: ", 4);
+	while(!done){		// Executes the code until done is true (see option '4' in the switch
+	int switchInput = askInt("\n\nPlease choose one of the following: \n\n1. Enter student's info. \n2. Print all the info saved. \n3. Search student by ID. \n4. Exit.\n\n Option: ", 4);
 		switch (switchInput) {
 			case 1:
 				printf("\n\nPlease Enter the following fields: \nName:");
 				scanf("%s", studentName);
 				strcpy(student[counter].name, studentName);
-				student[counter].grade = askInt("Grade: ", 100);
-				student[counter].idCard = askInt("ID: ", 200000000);
-				counter++;
+				student[counter].grade = askInt("Grade: ", 100);		// the second parameter on askInt() and askDouble() is the max value the user can enter.
+				student[counter].idCard = askInt("ID: ", 200000000);	// the second parameter on askInt() and askDouble() is the max value the user can enter.
+				counter++;												// used to count how many students there are (from the possible students that will fit into the array
 				break;
 			
 			case 2:
-				for (int i = 0; i < maxStudents; i++) {
-					printf("\n\nStudent's name: %s\n",	student[i].name);
+				for (int i = 0; i < maxStudents; i++) {					// 'for' used to print every student saved in the array
+					printf("\nStudent's name: %s\n",	student[i].name);
 					printf("Students's grade: %d\n",	student[i].grade);
 					printf("Student's ID: %d\n",	student[i].idCard);
-				} done = true;
+				}
 				break;
 			
 			case 3:
+				id = askInt("\nPlease enter student's ID. \nID: ", 200000000);				// asks the user for the ID number
+				for (int i = 0; i < maxStudents; i++) {
+					if (id == student[i].idCard) {											// checks if there's an existing id that matches the input
+						printf("\nFound who you are searching for. \nStudent: %s\n", student[i].name);
+						noMatch = false;
+					}
+				}
+				if (noMatch)
+					printf("\nNo match found. Returning...");
 				break;
 			
 			case 4:
-				done = true;
+				done = true;		// exits the main menu and thus, ends the program
 				break;
 		}
 	}
